8b765aef0b88a799665be8621ae95439a3adebe4
Todd H. Gardner <todd@wocketware.com>
2014-02-04 14:12:56 -0600

    add-in to wrap element addEventListener errors

---
+++ public/js/wrapListenerErrors.js
@@ -0,0 +1,21 @@
+(function (window, document) {
+
+  var originalAddEventListener = Element.prototype.addEventListener;
+
+  Element.prototype.addEventListener = function wrappedAddEventListener(event, callback, onerror) {
+
+    originalAddEventListener.call(this, event, function() {
+      try {
+        callback();
+      } catch(error) {
+        if (typeof onerror === 'function') {
+          onerror(error);
+        } else {
+          throw error;
+        }
+      }
+    });
+
+  };
+
+})(window, document);
