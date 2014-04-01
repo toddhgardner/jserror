8046a1506679058dbb5d4d67e2f310706bbe53e9
Todd H. Gardner <todd@wocketware.com>
2014-04-01 18:59:48 -0500

    added printprops support for simple types

--- public/js/util.js
+++ public/js/util.js
@@ -8,31 +8,37 @@ function printProps(error, element) {
   element.innerHTML = "<table class='table'><thead><tr><th>Prop</th><th>Value</th></tr></thead><tbody id='error-table-"+ errorElCounter +"'></tbody></table>";
   var elementTable = document.getElementById("error-table-"+ errorElCounter);
 
-  getAllProperties(error).forEach(function (prop) {
-    innerElCounter++;
+  if (typeof error !== 'object') {
+    elementTable.innerHTML += "<tr><td></td><td>"+error+"</td></tr>";
+  }
 
-    if (prop === 'stack') {
-      hasStack = true;
+  else {
+    getAllProperties(error).forEach(function (prop) {
+      innerElCounter++;
+
+      if (prop === 'stack') {
+        hasStack = true;
+      }
+
+      if (typeof error[prop] === "function") {
+        return;
+      }
+
+      if (typeof error[prop] === "object" && prop !== "__proto__") {
+        elementTable.innerHTML += "<tr><td>"+prop+"</td><td id='inner-error-"+innerElCounter+"'></td></tr>";
+        var innerTableEl = document.getElementById("inner-error-"+ innerElCounter);
+        printProps(error[prop], innerTableEl);
+      }
+      else {
+        elementTable.innerHTML += "<tr><td>"+prop+"</td><td>"+error[prop].toString().replace(/(\r\n|\n|\r)/gm,"<br>").replace(/(\t|\s\s)/gm,"&nbsp;&nbsp;")+"</td></tr>";
+      }
+
+    });
+
+    // weird Mozilla issue where the stack property doesn't enumerate, but it does exist
+    if (!hasStack && error.stack) {
+      elementTable.innerHTML += "<tr><td>stack</td><td>"+error.stack.toString().replace(/(\r\n|\n|\r)/gm,"<br>").replace(/(\t|\s\s)/gm,"&nbsp;&nbsp;")+"</td></tr>";
     }
-
-    if (typeof error[prop] === "function") {
-      return;
-    }
-
-    if (typeof error[prop] === "object" && prop !== "__proto__") {
-      elementTable.innerHTML += "<tr><td>"+prop+"</td><td id='inner-error-"+innerElCounter+"'></td></tr>";
-      var innerTableEl = document.getElementById("inner-error-"+ innerElCounter);
-      printProps(error[prop], innerTableEl);
-    }
-    else {
-      elementTable.innerHTML += "<tr><td>"+prop+"</td><td>"+error[prop].toString().replace(/(\r\n|\n|\r)/gm,"<br>").replace(/(\t|\s\s)/gm,"&nbsp;&nbsp;")+"</td></tr>";
-    }
-
-  });
-
-  // weird Mozilla issue where the stack property doesn't enumerate, but it does exist
-  if (!hasStack && error.stack) {
-    elementTable.innerHTML += "<tr><td>stack</td><td>"+error.stack.toString().replace(/(\r\n|\n|\r)/gm,"<br>").replace(/(\t|\s\s)/gm,"&nbsp;&nbsp;")+"</td></tr>";
   }
 
 }
